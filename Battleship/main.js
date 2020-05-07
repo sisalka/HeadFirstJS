3703e8914d9cf69228246226416c8e05f58747bf
sisalka <hellosjuloves@gmail.com>
2020-05-07 12:25:23 +0200

    Fixed errors

--- Battleship/main.js
+++ Battleship/main.js
@@ -10,16 +10,16 @@ var isSunk=false;
 
 while (isSunk==false){
   guess=prompt("have guess from 0 to 6");
-  if ((guess < 0 or guess >6) or isNaN(guess)){
+  if ((guess < 0 || guess >6) || isNaN(guess)){
       guess=prompt("must be a number from 0 to 6");
   }else{
     guesses +=1;
   }
-    if (guess == loc1 or guess==loc3 or guess == loc2){
+    if (guess == loc1 || guess==loc3 || guess == loc2){
       hits +=1;
     }
     if (hits ==3){
-      isSunk==true;
+      isSunk=true;
       alert("you won");
     }
 }
